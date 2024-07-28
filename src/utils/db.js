85fe44b4fd15357d5449307aa62c93635b0db036
THARUKA-D <tharukajn@gmail.com>
2024-07-28 07:24:08 +0530

    Added husky

--- src/utils/db.js
+++ src/utils/db.js
@@ -7,7 +7,7 @@ const client = new MongoClient(uri);
 const getMongoClient = async () => {
   try {
     await client.connect();
-    return client;
+                 return client;
   } catch (error) {
     console.error('Error connecting to MongoDB:', error);
     throw error;
